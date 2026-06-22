9ad9e4671bdb71063a4f07441c56562c0479d30d
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-06-22 15:23:29 +0000

    aaaaaaaaaaaaaaaaaaaaaaa

---
+++ for.js
@@ -0,0 +1,10 @@
+
+
+const numero = [100, 200, 300, 400, 500, 600]
+//primeira expressão: é executada apenas uma única vez
+//segunda expressão: condição de execução
+// terceira expressão: é executada sempre ao final do bloco
+
+for (let index = 0; indice < numeros.lenght; indice++) {
+    console.log(numeros[indice])
+}
---
+++ media-for.js
@@ -0,0 +1,6 @@
+const notas = [10, 6.5, 8, 7.5]
+
+//const media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+
+for (let indice = 0; indice < notas.length; i++) {
+    
