71a4681bf2b679ddecb68865f97b894415b40be0
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-06-24 14:54:07 +0000

    atividade

--- media-for.js
+++ media-for.js
@@ -2,5 +2,11 @@ const notas = [10, 6.5, 8, 7.5]
 
 //const media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
 
-for (let indice = 0; indice < notas.length; i++) {
-    
+let somaDaNotas = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    somaDaNotas += notas [i];
+}
+console.log(somaDaNotas / notas.length);
+
+console.log('A Média das notas é ${media}.');
