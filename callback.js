b93f430b8b0fb7110d42ec75d2b66e072f4f477a
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-07-01 15:17:22 +0000

    exemplo

---
+++ callback.js
@@ -0,0 +1,17 @@
+const nomes = ["Alice", "Bob", "Charlie"]
+
+//nomes.forEach((nome) => {
+   //console.log(nome);
+//}];
+
+//nomes.forEach((nome) => {
+   //console.log(nome);
+//}];
+
+function imprimirNomes(nomes) {
+    nomes.forEach((nome) => {
+        console.log(nome);
+    });
+}
+
+imprimirNomes(nomes);
