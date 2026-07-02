ab3aec9b7d9a20ab5acf9486c47d8638f0a58087
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-07-02 15:23:49 +0000

    remap-nomes

---
+++ map-nomes.js
@@ -0,0 +1,6 @@
+const nomes = ["ana", "beatriz", "carlos", "DANIELA"]
+    const nomesPadronuizados = nomes.map((nome) =>{
+        return nome .toLowerCase();
+    })
+
+    console.log(nomesPadronuizados)
