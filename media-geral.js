1d270de3facd112ffe2ae27b21e34083a08640c8
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-08-06 15:23:02 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- media-geral.js
+++ media-geral.js
@@ -3,10 +3,15 @@ const salaJava = [6, 5, 8, 9, 5, 6];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(notasDaSala) {
-    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => acumulador + nota, 0);
+    const somaDasNotas = notasDaSala.reduce((acc, nota) => acc + nota, 0);
     const media = somaDasNotas / notasDaSala.length;
-    console.log(media);
+    return media;
 }
+console.log('A média da sala JavaScript é $(calculaMedia(salaaJS)}')
+console.log('A média da sala Java é $(calculaMedia(salaJava)}')
+console.log('A média da sala Python é $(calculaMedia(salaPython)}')
+
+
 
 calculaMedia(salaJS);
 calculaMedia(salaJava);
